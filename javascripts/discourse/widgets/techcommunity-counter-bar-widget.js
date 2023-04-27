3de2ca65d87f90288d38982e11327bf2d4baa8f8
webdirektindia <webdirektindia@gmail.com>
2023-04-27 16:08:10 +0530

    Purple bar changes: 1. Replace the numbers with the font awesome icons. 2. For the Community Experts tab you can use the users' icon 3. Add a new column in the Purple bar for the "Feedback" category 4. These changes will be done in the techcommunity-banners component. 5. Change the sequence of the categories

--- javascripts/discourse/widgets/techcommunity-counter-bar-widget.js
+++ javascripts/discourse/widgets/techcommunity-counter-bar-widget.js
@@ -1,4 +1,5 @@
 import { createWidget } from "discourse/widgets/widget";
+import { iconHTML, iconNode } from "discourse-common/lib/icon-library";
 import { h } from "virtual-dom";
 
 export default createWidget("techcommunity-counter-bar-widget", {
@@ -15,14 +16,104 @@ export default createWidget("techcommunity-counter-bar-widget", {
 
         if (showOnHomepage) {
             document.querySelector("body").classList.add("show-counter-bar");
+            
+            const counterBarColumnLinks = settings.counter_bar_columns;
+            if (!counterBarColumnLinks.length) {
+                return;
+            }
+            let links = [];
+
+            counterBarColumnLinks
+                .split("|")
+                .filter(Boolean)
+                .map((counterBarLinksArray, index) => {
+                    const [
+                        linkText,
+                        linkHref,
+                        icon,
+                    ] = counterBarLinksArray
+                        .split(",")
+                        .filter(Boolean)
+                        .map((x) => x.trim());
+                
+                    const anchorAttributes = {
+                        href: linkHref,
+                    };
+
+                    links.push(
+                        h("div.counter-bar-column.column" + (index+1), 
+                            h("a.column_link", anchorAttributes, [
+                                    h("span.category-icon", {}, icon !== "" ? iconNode(icon) : ""),
+                                    h("span", { innerHTML: linkText })
+                                ]
+                            )
+                        )
+                    );
+                });
+                
+            return h(`div.counter-bar`, links);
+
+            /*
             return h(
                 `div.counter-bar`, [
-                    h("div.counter-bar-column.column1", h("a.column1_link", { innerHTML: settings.counter_bar_column1_text, href: settings.counter_bar_column1_link})),
-                    h("div.counter-bar-column.column2", h("a.column1_link", { innerHTML: settings.counter_bar_column2_text, href: settings.counter_bar_column2_link})),
-                    h("div.counter-bar-column.column3", h("a.column1_link", { innerHTML: settings.counter_bar_column3_text, href: settings.counter_bar_column3_link})),
-                    h("div.counter-bar-column.column4", h("a.column1_link", { innerHTML: settings.counter_bar_column4_text, href: settings.counter_bar_column4_link}))
+                    h("div.counter-bar-column.column1", 
+                        h("a.column_link", 
+                            { 
+                                
+                                href: settings.counter_bar_column1_link
+                            }, [
+                                h("span.category-icon", {}, settings.counter_bar_column1_icon !== "" ? iconNode(settings.counter_bar_column1_icon) : ""),
+                                h("span", { innerHTML: settings.counter_bar_column1_text })
+                            ]
+                        )
+                    ),
+                    h("div.counter-bar-column.column2", 
+                        h("a.column_link", 
+                            { 
+                                    
+                                href: settings.counter_bar_column2_link
+                            }, [
+                                h("span.category-icon", {}, settings.counter_bar_column2_icon !== "" ? iconNode(settings.counter_bar_column2_icon) : ""),
+                                h("span", { innerHTML: settings.counter_bar_column2_text })
+                            ]
+                        )
+                    ),
+                    h("div.counter-bar-column.column3", 
+                        h("a.column_link", 
+                            { 
+                                    
+                                href: settings.counter_bar_column3_link
+                            }, [
+                                h("span.category-icon", {}, settings.counter_bar_column3_icon !== "" ? iconNode(settings.counter_bar_column3_icon) : ""),
+                                h("span", { innerHTML: settings.counter_bar_column3_text })
+                            ]
+                        )
+                    ),
+                    h("div.counter-bar-column.column4", 
+                        h("a.column_link", 
+                            { 
+                                    
+                                href: settings.counter_bar_column4_link
+                            }, [
+                                h("span.category-icon", {}, settings.counter_bar_column4_icon !== "" ? iconNode(settings.counter_bar_column4_icon) : ""),
+                                h("span", { innerHTML: settings.counter_bar_column4_text })
+                            ]
+                        )
+                    ),
+                    h("div.counter-bar-column.column4", 
+                        h("a.column_link", 
+                            { 
+                                    
+                                href: settings.counter_bar_column5_link
+                            }, [
+                                h("span.category-icon", {}, settings.counter_bar_column5_icon !== "" ? iconNode(settings.counter_bar_column5_icon) : ""),
+                                h("span", { innerHTML: settings.counter_bar_column5_text })
+                            ]
+                        )
+                    )
                 ]
             );
+            */
         }
         else {
             //Do not display Counter bar
